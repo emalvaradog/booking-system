3fa67cbfa629fbc8b02ad7b5682aa6ded4c7112f
emanuelalvaradog <emanuel.alvarado.g@gmail.com>
2023-06-27 16:17:18 -0600

    chore: install useful dependencies

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,6 +1,8 @@
 import initAuth from "@/firebase/config";
 import { store } from "@/store/store";
 import "@/styles/globals.css";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { Inter } from "next/font/google";
 import { Provider } from "react-redux";
 
@@ -10,10 +12,12 @@ initAuth();
 
 export default function App({ Component, pageProps }) {
   return (
-    <Provider store={store}>
-      <main className={inter.className}>
-        <Component {...pageProps} />
-      </main>
-    </Provider>
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Provider store={store}>
+        <main className={inter.className}>
+          <Component {...pageProps} />
+        </main>
+      </Provider>
+    </LocalizationProvider>
   );
 }
